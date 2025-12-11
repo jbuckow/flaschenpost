cfee048f8ba76c2f60349dc8108f4c66baa363bb
Jan-Niklas Buckow <j.buckow@live.de>
2025-12-11 11:17:13 +0100

    feature to read in scheduling instances

--- scheduling/scheduling.jl
+++ scheduling/scheduling.jl
@@ -1,5 +1,30 @@
 using DataStructures
 
+# Instance of P||Cmax
+struct Instance
+    m::Int                  # number of machines
+    n::Int                  # number of jobs
+    p::Vector{Int}          # processing times p_i
+end
+
+# Solution of P||Cmax
+struct Solution
+    assignment::Vector{Int} # machine assignment for each job (length n)
+    makespan::Int           # resulting makespan
+end
+
+# Reads an instance from a file
+function read_instance(path::String)
+    lines = readlines(path)
+    # Parse first two lines
+    m = parse(Int, lines[1])
+    n = parse(Int, lines[2])
+    # Parse processing times
+    p = [parse(Int, lines[i]) for i in 3:(2+n)]
+    # Create the instance
+    return Instance(m, n, p)
+end
+
 function calculate_ub(m::Int, pjs::Vector{Int})
     # Sortiere Jobs absteigend (Largest Processing Time zuerst)
     #sort!(pjs, rev=true)
@@ -24,10 +49,22 @@ function calculate_ub(m::Int, pjs::Vector{Int})
     return max_load
 end
 
+function main()
+    # Check the command line arguments
+    if length(ARGS) < 1
+        println("Usage: julia scheduling.jl <instance-file>")
+        return
+    end
 
+    # Read the instance
+    filepath = ARGS[1]
+    inst = read_instance(filepath)
+    
+    # Print information about the instance
+    println("Number of machines: ", inst.m)
+    println("Number of jobs: ", inst.n)
+    println("Processing times: ", inst.p)
+end
 
-jobs = [12, 4, 18, 5, 29]
-m = 3
-
-ub = calculate_ub(m, jobs)
-println("Upper Bound: ", ub)
+# Start the main function
+main()
